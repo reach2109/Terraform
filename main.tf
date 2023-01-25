3b7f260813c69941a1e4d524309fff9a562d3193
Reachable Now <reachable_now@outlook.com>
2023-01-25 16:36:40 +1100

    Security Group added before Commit

--- main.tf
+++ main.tf
@@ -3,11 +3,29 @@ provider "aws" {
 }
 
 resource "aws_instance" "terratest" {
-ami           = "ami-0a606d8395a538502"
-instance_type = "t2.micro"
+ami                    = "ami-0ab0629dba5ae551d"
+instance_type          = "t2.micro"
+vpc_security_group_ids = [aws_security_group.instance.id]
 
+user_data = <<-EOF
+            #!/bin/bash
+            echo "Hello World" > index.html
+            nohup busybox httpd -f -p 8080 &
+            EOF
 tags = {
 Name   = "Terraform-Example"
 Branch = "Learning"
+ }
 }
-}
+
+resource "aws_security_group" "instance" {
+  name = "terraform-example-instance"
+
+  ingress {
+    from_port = "8080"
+    to_port   = "8080"
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+   }
+} 
+  
